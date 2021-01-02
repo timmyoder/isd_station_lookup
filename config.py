efb95c348d86ac4fd159a821773ead2a2816a376
Tim Yoder <tim.a.yoder@gmail.com>
2021-01-02 11:50:47 -0800

    select closest from db

--- config.py
+++ config.py
@@ -5,7 +5,8 @@ import pathlib
 project_root = pathlib.Path(__file__).parent
 
 RESOURCE_DIR = project_root / 'resources'
+INPUT_DIR = project_root / 'input_data'
 
-for folder in [RESOURCE_DIR]:
+for folder in [RESOURCE_DIR, INPUT_DIR]:
     if not folder.exists():
         folder.mkdir()
--- lookup.py
+++ lookup.py
@@ -4,37 +4,65 @@ import datetime as dt
 import time
 
 import peewee as pw
+from playhouse.sqlite_udf import sqrt
 from geopy import distance as geo_dist
 
 from models import StationHistory
+from config import INPUT_DIR
 
 
-def select_close_stations(lat_target, lon_target, active_only=True):
-    """selects stations from the database within +/- 1 degree lat and lon."""
-    query = StationHistory.select().where(
-        StationHistory.LON.between(lon_target - 1, lon_target + 1) &
-        StationHistory.LAT.between(lat_target - 1, lat_target + 1)
-    )
+def select_closest_stations(lat_target, lon_target, active_only=True):
+    """
+    selects the 100 closest stations to the target location
 
+     Distance calculated as the square of the euclidean distance
+     between station locations and target location.
+
+     distances = (d_LAT)^2 + (d_LON)^2
+
+     """
+    query = StationHistory.select(
+        StationHistory.USAF,
+        StationHistory.WBAN,
+        StationHistory.STATION_NAME,
+        StationHistory.LAT,
+        StationHistory.LON,
+        StationHistory.BEGIN,
+        StationHistory.END,
+        StationHistory.CTRY,
+        StationHistory.STATE,
+        StationHistory.ELEV,
+        StationHistory.ICAO,
+        ((StationHistory.LON - lon_target) * (StationHistory.LON - lon_target) +
+         (StationHistory.LAT - lat_target) * (StationHistory.LAT - lat_target)
+         )
+    ).order_by(
+        ((StationHistory.LON - lon_target) * (StationHistory.LON - lon_target) +
+         (StationHistory.LAT - lat_target) * (StationHistory.LAT - lat_target))
+    ).where(
+        ~StationHistory.LON.is_null()
+    ).limit(100)
     close_stations = pd.DataFrame(list(query.dicts()))
+    close_stations.columns = ['USAF', 'WBAN', 'STATION_NAME', 'LAT',
+                              'LON', 'BEGIN', 'END', 'CTRY',
+                              'STATE', 'ELEV', 'ICAO', 'dist']
 
     if active_only:
         two_wks_ago = dt.date.today() - dt.timedelta(weeks=2)
         active_stns = close_stations[close_stations['END'] > two_wks_ago]
-        return active_stns
-    return close_stations
-
-
-def calc_distance_euclidean(locations, lat_target, lon_target):
-    locations['distance'] = np.sqrt(
-        np.square(locations['LON'] - lon_target) +
-        np.square(locations['LAT'] - lat_target)
-    )
-    return locations
+        return active_stns.reset_index(drop=True).drop(columns=['dist'])
+    return close_stations.drop(columns=['dist'])
 
 
 def calc_distance_actual(locations, lat_target, lon_target):
-    locations['distance'] = locations.apply(
+    """Actualates the actual distance (in miles) betwee the target lat/lon
+    and the stations' location
+
+    Uses geopy.geo_dist.distance. Two or three times slower than
+    calc_distance_euclidean()
+    """
+
+    locations['distance_miles'] = locations.apply(
         lambda x: geo_dist.distance((x['LAT'], x['LON']),
                                     (lat_target, lon_target),
                                     ).miles,
@@ -46,38 +74,58 @@ def calc_distance_actual(locations, lat_target, lon_target):
 def find_closest(lat_target,
                  lon_target,
                  active_only=True,
-                 distance_type='actual'):
-    """finds the closest station as defined by euclidean distance"""
-    close_stations = select_close_stations(lat_target=lat_target,
-                                           lon_target=lon_target,
-                                           active_only=active_only)
+                 return_tuple=False):
+    """finds the closest station as defined
 
-    if distance_type == 'actual':
-        close_stations = calc_distance_actual(close_stations,
-                                              lat_target,
-                                              lon_target)
-    elif distance_type == 'euclidean':
-        calc_distance_euclidean(close_stations,
-                                lat_target,
-                                lon_target)
-    else:
-        raise ValueError('Distance type must be "actual" or "euclidean"')
+    Performance of distance_type method
+    actual: 0.019243001000000093 s
+    euclid: 0.008490397999999955 s
+    """
+    close_stations = select_closest_stations(lat_target=lat_target,
+                                             lon_target=lon_target,
+                                             active_only=active_only)
+
+    close_stations = calc_distance_actual(close_stations,
+                                          lat_target,
+                                          lon_target)
 
     # remove stations with USAF=999999
     close_stations = close_stations[close_stations['USAF'] != '999999']
 
-    closest = close_stations.loc[close_stations.distance ==
-                                 close_stations['distance'].min()]
-    return closest
+    closest = close_stations.loc[close_stations.distance_miles ==
+                                 close_stations['distance_miles'].min()]
+    if not return_tuple:
+        return closest
+    return (closest['USAF'].values[0],
+            closest['WBAN'].values[0],
+            closest['distance_miles'].values[0])
+
+
+def find_closest_csv(file_name, active_only=True, distance_type='actual'):
+    input_file = INPUT_DIR / file_name
+    if not input_file.is_file():
+        raise ValueError(f'{file_name} not found in {INPUT_DIR}')
+
+    points = pd.read_csv(input_file)
+
+    if 'Latitude' not in points.columns or 'Longitude' not in points.columns:
+        raise ValueError('Input file must have columns with the labels: "Latitude" and "Longitude"')
+
+    stns = pd.DataFrame(points.apply(
+        lambda x: find_closest(lat_target=x['Latitude'],
+                               lon_target=x['Longitude'],
+                               active_only=active_only,
+                               return_tuple=True),
+        axis=1).tolist(),
+                        columns=['USAF', 'WBAN', 'distance_miles'],
+                        index=points.index)
+
+    labeled_points = pd.concat([points, stns], axis=1)
+
+    return labeled_points
 
 
 if __name__ == '__main__':
-    t0 = time.perf_counter()
-
-    e = find_closest(47.651914, -122.343461, active_only=True, distance_type='actual')
-    t1 = time.perf_counter()
-    print(f'euclidean: {t1 - t0}')
-
-    act = find_closest(47.651914, -122.343461, active_only=True, distance_type='euclidean')
-    t2 = time.perf_counter()
-    print(f'actual: {t2 - t1}')
+    rand = find_closest_csv('rand.csv', distance_type='actual')
+    # t = select_closest_stations(47.651910, -122.343435)
+    # t = find_closest(47.651910, -122.343435)
